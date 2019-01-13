52242235b12453bb98a0b4fb3728a2a39df8d229
kratostaine <madhu.bhat.8j@gmail.com>
2019-01-13 10:46:28 +0530

    Updated gitignore and added routes on a separate file

--- controller/controller.go
+++ controller/controller.go
@@ -2,10 +2,7 @@ package controller
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
 type Controller struct {
@@ -16,41 +13,4 @@ func (c *Controller) getHealth(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write(data)
 	return
-}
-
-type Route struct {
-	Name        string
-	Method      string
-	Pattern     string
-	HandlerFunc http.HandlerFunc
-}
-
-type Routes []Route
-
-var controller = &Controller{}
-
-var routes = Routes{
-	Route{
-		"Health",
-		"GET",
-		"/health",
-		controller.getHealth,
-	},
-}
-
-func NewRouter() *mux.Router {
-	router := mux.NewRouter().StrictSlash(true)
-	log.Println("Registered routes:")
-	for _, route := range routes {
-		var handler http.Handler
-		log.Println(route.Name + ": " + route.Method + " " + route.Pattern)
-		handler = route.HandlerFunc
-
-		router.
-			Methods(route.Method).
-			Path(route.Pattern).
-			Name(route.Name).
-			Handler(handler)
-	}
-	return router
 }
